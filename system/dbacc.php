f3d44e88565900c434338e414ab7830de889392c
Charlie FG <charlie@carrotsrc.org>
2014-07-11 11:05:14 +0100

    Notice update

--- system/dbacc.php
+++ system/dbacc.php
@@ -1,5 +1,5 @@
 <?php
-/* (C)opyright 2014, Carrotsrc.org
+/* Copyright 2014, Charlie Fyvie-Gauld (Carrotsrg.org)
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
